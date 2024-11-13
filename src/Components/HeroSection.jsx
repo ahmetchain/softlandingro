286f00be38636e5302499bef79a0f87627e0a2db
Ahmet <ahmetarapaslan@gmail.com>
2024-11-13 13:34:01 +0300

    fix:

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,27 +1,36 @@
 import React from "react";
-import { FaGlobe, FaRocket } from "react-icons/fa";
 import content from "../../content.json";
 
 const HeroSection = () => {
   return (
     <div className="w-full bg-white mt-[70px]">
       <div className="flex flex-col items-center mx-auto md:flex-row max-w-[1320px] md:h-[400px] p-4">
-        {/* Başlık ve Giriş Metni */}
         <div className="text-center md:text-left md:w-[800px]">
-          <h1 className="text-2xl sm:text-3xl md:text-[43px] font-bold text-[#02094d] leading-tight">
+          <h1 className="text-2xl sm:text-3xl md:text-[43px] font-bold text-[#02094d] leading-loose">
             {content.tagline}
           </h1>
+          <h1 className="text-2xl  lg:mt-2 sm:text-3xl md:text-[43px] font-bold text-[#02094d] leading-loose">
+            {content.tagline2}
+          </h1>
+          <h1 className="text-2xl  lg:mt-2 sm:text-3xl md:text-[43px] font-bold text-[#02094d] leading-loose">
+            {content.tagline3}
+          </h1>
+
           <p className="mt-6 sm:mt-8 text-lg sm:text-xl text-gray-600">
             {content.introText}
           </p>
         </div>
 
-        {/* Yalnızca Dünya Simgesi (Mobilde Gizli) */}
         <div className="hidden md:flex justify-center md:ml-20 mt-8 md:mt-0">
-          <div className="relative w-[180px] h-[180px] lg:w-[250px] lg:h-[250px]">
-            <div className="rounded-full w-full h-full flex items-center justify-center bg-gradient-to-r from-blue-400 to-[#02094d] shadow-lg">
-              <FaGlobe className="text-white text-[60px] lg:text-[80px]" />
-            </div>
+          <div className="relative w-[180px] h-[180px] lg:w-[350px] lg:h-[350px]">
+            <lottie-player
+              src="https://lottie.host/bbb0d610-bb62-476d-857f-51f1185330aa/QMx6o9OZrK.json"
+              background="transparent"
+              speed="0.4"
+              loop
+              autoplay
+              style={{ position: "absolute", width: "100%", height: "100%" }}
+            ></lottie-player>
           </div>
         </div>
       </div>
